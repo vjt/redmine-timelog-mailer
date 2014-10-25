0bb7d64529948ef994ce8384dd02e586ac671dad
Marcello Barnaba <vjt@openssl.it>
2014-10-25 09:58:28 +0200

    Version 0.0.4

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :timelog_mailer do
   name 'Time Log Mailer'
   author 'Marcello Barnaba'
   description 'E-mails project members when time entries are logged'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/vjt/redmine-timelog-mailer'
   author_url 'http://sindro.me'
 
